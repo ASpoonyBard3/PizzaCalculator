d6b8c76072bfb7de445b2248dbbf896f86acc72e
Gatlingover <Samsonf21@gmail.com>
2017-08-31 17:07:45 +0100

    First committee using weird cmd trick

---
+++ PizzaCalc/PizzaCalc.py
@@ -0,0 +1,15 @@
+import matplotlib
+import os
+import flask
+from math import exp
+
+#collect user input of pizzaOption1
+pizzaOption1 = 
+
+# collect user input of pizzaOption2
+pizzaOption2 = 
+
+pizzaPi = 3.142
+
+#compare pizzaOption2 vs PizzaOption1
+print (pizzaPi*(pizzaOption1/2)**2)
